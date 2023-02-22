fda9bf8980c48064df21f42f17aaf85f6b258f49
vvarcos <viniciuussiilva@gmail.com>
2023-02-22 11:10:19 -0300

    Criando as urls

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Marcos Vinícius',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Marcos Vinícius',
+    })
