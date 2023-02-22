7c5071460e7871574d0463630117ec38168fdf6b
vvarcos <viniciuussiilva@gmail.com>
2023-02-22 19:11:59 -0300

    Alterações no CSS e criação do DB

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -14,6 +14,7 @@ fake = Faker('pt_BR')
 
 def make_recipe():
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
@@ -27,10 +28,10 @@ def make_recipe():
                 'last_name': fake.last_name(),
         },
         'category': {
-                    'name': fake.word()
+                    'name': fake.word().capitalize
         },
         'cover': {
-                'url':'https://loremflickr.com/640/320/food,cock'
+                'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
         }
 
     }
