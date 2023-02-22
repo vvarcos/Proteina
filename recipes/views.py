408c9320c1c968b7afa73774f4606169f596ed4f
vvarcos <viniciuussiilva@gmail.com>
2023-02-22 12:18:24 -0300

    Lista de receitas

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Marcos Vinícius',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Marcos Vinícius',
+        'recipe': make_recipe,
     })
